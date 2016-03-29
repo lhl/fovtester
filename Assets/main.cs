f70852bb5fdc3ae7dd73644e18f7ca0aa3cdeabb
lhl <lhl@randomfoo.net>
2016-03-29 12:04:48 -0700

    new project

---
+++ Assets/main.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+/* 
+
+TODO:
+* Main Scene
+* 120 degree Quad
+* Recenter
+* Locking
+* Support for:
+  * CV1
+  * DK2
+  * Vive
+* Controller Projection (Raycast Pointer)
+* Painting
+* Export Painted Image
+* Detect HMD
+
+
+
+
+    
+    
+    
+Analytics - Detect Which HMD
+https://analytics.cloud.unity3d.com/integration/b89d34b1-cb46-4cff-a475-88c8d9d4dd3d?unity_version=
+
+// Reference the Unity Analytics namespace
+using UnityEngine.Analytics;
+
+// Use this call for wherever a player triggers a custom event
+Analytics.CustomEvent(string customEventName,
+IDictionary<string, object> eventData);
+
+*/
